04e141db626454760833f2ef201a039a6f1f4ccb
Connor Priester <cipriester01@gmail.com>
2024-03-14 18:50:31 -0500

    Added Normal class to hold original tick

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -21,11 +21,8 @@ class GildedRose
   end
 
   def normal_tick
-    @days_remaining -= 1
-    return if @quality == 0
-
-    @quality -= 1
-    @quality -= 1 if @days_remaining <= 0
+    @item = Normal.new(quality, days_remaining)
+    item.tick
   end
 
   def brie_tick
@@ -34,6 +31,16 @@ class GildedRose
 
     @quality += 1
     @quality += 1 if @days_remaining <= 0
+  end
+
+  def quality
+    return item.quality if item
+    @quality
+  end
+
+  def days_remaining
+    return item.days_remaining if item
+    @days_remaining
   end
 
   def sulfuras_tick
@@ -47,5 +54,21 @@ class GildedRose
     @quality += 1
     @quality += 1 if @days_remaining < 10
     @quality += 1 if @days_remaining < 5
+  end
+
+  class normal
+    attr_reader :quality, :days_remaining
+
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+
+      @quality -= 1
+      @quality -= 1 if @days_remaining <= 0
+    end
   end
 end
