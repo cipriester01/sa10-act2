c21f46395668a226b3d7e70c2653210559fa597f
Connor Priester <cipriester01@gmail.com>
2024-03-14 18:26:02 -0500

    Added brie tick

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -57,6 +57,7 @@ class GildedRose
       end
     end
   end
+
   def normal_tick
     @days_remaining -= 1
     return if @quality == 0
@@ -65,6 +66,12 @@ class GildedRose
     @quality -= 1 if @days_remaining <= 0
   end
 
+  def brie_tick
+    @days_remaining -= 1
+    return if @quality >= 50
 
+    @quality += 1
+    @quality += 1 if @days_remaining <= 0
+  end
 
 end
