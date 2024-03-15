fe4b7387ebb2cb650c5e6181562816e59de4fcd0
Connor Priester <cipriester01@gmail.com>
2024-03-14 19:10:14 -0500

    Added sulfuras class

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -41,6 +41,8 @@ class GildedRose
   end
 
   def sulfuras_tick
+    @item = Sulfuras.new(quality, days_remaining)
+    item.tick
   end
 
   def backstage_tick
@@ -84,4 +86,16 @@ class GildedRose
       @quality += 1 if @days_remaining <= 0
     end
   end
+
+  class Sulfuras
+    attr_reader :quality, :days_remaining
+
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+
+    def tick
+    end
+  end
+
 end
