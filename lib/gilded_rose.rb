5f41345c5b127658d98b26a79fb8202b4e85854c
Connor Priester <cipriester01@gmail.com>
2024-03-14 19:44:56 -0500

    DONT remove everything. i am stupid

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,25 +1,55 @@
 module GildedRose
-
-  def self.for(name, quality, days_remaining)
-    klass_for(name).new(quality, days_remaining)
-  end
-
   class Item
+    attr_reader :quality, :days_remaining
+
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+
     def tick
     end
   end
 
-  def self.klass_for(name)
-    case name
-    when 'normal'
-      Normal
-    when 'Aged Brie'
-      Brie
-    when 'Sulfuras, Hand of Ragnaros'
-      Item
-    when 'Backstage passes to a TAFKAL80ETC concert'
-      Backstage
+  class Normal < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+
+      @quality -= 1
+      @quality -= 1 if @days_remaining <= 0
     end
   end
 
+  class Brie < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50
+
+      @quality += 1
+      @quality += 1 if @days_remaining <= 0
+    end
+  end
+
+  class Backstage < Item
+    def tick
+    @days_remaining -= 1
+    return              if @quality >= 50
+    return @quality = 0 if @days_remaining < 0
+
+    @quality += 1
+    @quality += 1 if @days_remaining < 10
+    @quality += 1 if @days_remaining < 5
+    end
+  end
+
+  DEFAULT_CLASS = Item
+  SPECIALIZED_CLASSES = {
+    'normal'                                    => Normal,
+    'Aged Brie'                                 => Brie,
+    'Backstage passes to a TAFKAL80ETC concert' => Backstage }
+
+  def self.for(name, quality, days_remaining)
+    (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS)
+      new(quality, days_remaining)
+  end
 end
