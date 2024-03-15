37613d48fae04f5fa64e425482ff78c8b356d193
Connor Priester <cipriester01@gmail.com>
2024-03-14 19:35:54 -0500

    Removed everything???

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -4,6 +4,11 @@ module GildedRose
     klass_for(name).new(quality, days_remaining)
   end
 
+  class Item
+    def tick
+    end
+  end
+
   def self.klass_for(name)
     case name
     when 'normal'
@@ -11,55 +16,9 @@ module GildedRose
     when 'Aged Brie'
       Brie
     when 'Sulfuras, Hand of Ragnaros'
-      Sulfuras
+      Item
     when 'Backstage passes to a TAFKAL80ETC concert'
       Backstage
-    end
-  end
-
-
-  class Item
-    attr_reader :quality, :days_remaining
-
-    def initialize(quality, days_remaining)
-      @quality, @days_remaining = quality, days_remaining
-    end
-  end
-
-  class Normal < Item
-    def tick
-      @days_remaining -= 1
-      return if @quality == 0
-
-      @quality -= 1
-      @quality -= 1 if @days_remaining <= 0
-    end
-  end
-
-  class Brie < Item
-    def tick
-      @days_remaining -= 1
-      return if @quality >= 50
-
-      @quality += 1
-      @quality += 1 if @days_remaining <= 0
-    end
-  end
-
-  class Sulfuras < Item
-    def tick
-    end
-  end
-
-  class Backstage < Item
-    def tick
-    @days_remaining -= 1
-    return              if @quality >= 50
-    return @quality = 0 if @days_remaining < 0
-
-    @quality += 1
-    @quality += 1 if @days_remaining < 10
-    @quality += 1 if @days_remaining < 5
     end
   end
 
